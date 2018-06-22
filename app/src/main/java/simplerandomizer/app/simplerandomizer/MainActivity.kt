a486803c0c31c0845b06827e21bb22a4facd85b9
Rommel Azucena <jrommelazucena@gmail.com>
2018-06-22 08:26:00 +0800

    Implements a random number generator with skeleton UI

--- app/src/main/java/simplerandomizer/app/simplerandomizer/MainActivity.kt
+++ app/src/main/java/simplerandomizer/app/simplerandomizer/MainActivity.kt
@@ -2,11 +2,23 @@ package simplerandomizer.app.simplerandomizer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import simplerandomizer.app.simplerandomizer.extensions.random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionButton()
+    }
+
+    private fun setupActionButton() {
+        btnGenerate.setOnClickListener { generateRandomNumber() }
+    }
+
+    private fun generateRandomNumber() {
+        val generatedResult = (0..100).random()
+        result.text = generatedResult.toString()
     }
 }
